9c90b863b1dfd8fadbb79c3fd7407efff691f11c
Kratika Rajawat <kratikarajawat05@gmail.com>
2025-07-18 13:45:36 +0530

    Update packages and logging middleware, add .gitignore

--- frontend/src/middleware/log.js
+++ frontend/src/middleware/log.js
@@ -1,19 +1,22 @@
+import axios from 'axios';
+
+
+const ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJhamF5cHJhdGFwdG9tYXIzQGdtYWlsLmNvbSIsImV4cCI6MTc1MjgyNTYyOCwiaWF0IjoxNzUyODI0NzI4LCJpc3MiOiJBZmZvcmQgTWVkaWNhbCBUZWNobm9sb2dpZXMgUHJpdmF0ZSBMaW1pdGVkIiwianRpIjoiMTcxZTM5YTYtZDk0OS00MjI0LWI1MWItMGQ2MTlhZmM4ZDZhIiwibG9jYWxlIjoiZW4tSU4iLCJuYW1lIjoiYWpheSBwcmF0YXAgdG9tYXIiLCJzdWIiOiIyN2I0OGMyYS03ZTFiLTQ0Y2UtYmQ1YS1jNWY0ZGU4NTFlMzcifSwiZW1haWwiOiJhamF5cHJhdGFwdG9tYXIzQGdtYWlsLmNvbSIsIm5hbWUiOiJhamF5IHByYXRhcCB0b21hciIsInJvbGxObyI6ImE2MDIwNTIyMjI2MCIsImFjY2Vzc0NvZGUiOiJKcHplclEiLCJjbGllbnRJRCI6IjI3YjQ4YzJhLTdlMWItNDRjZS1iZDVhLWM1ZjRkZTg1MWUzNyIsImNsaWVudFNlY3JldCI6IkVZU0JndEVhVWdtalRLTm0ifQ.mOFcWpCJJImTagMmIyayNFygkKRqZMnneVe6NMIC17o";
+
 const log = async (stack, level, pkg, message) => {
-    try {
-      await fetch('http://20.244.56.144/evaluation-service/logs', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          stack,
-          level,
-          package: pkg,
-          message,
-        }),
-      })
-    } catch (err) {
-      console.error('Log error:', err)
-    }
+  try {
+    await axios.post(
+      "http://20.244.56.144/evaluation-service/logs",
+      { stack, level, package: pkg, message },
+      {
+        headers: {
+          Authorization: `Bearer ${ACCESS_TOKEN}`,
+        },
+      }
+    );
+  } catch (err) {
+    console.error("Failed to log:", err.message);
   }
-  
-  export default log
-  
+};
+
+export default log;
